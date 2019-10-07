e413b6304cae9672376bb2067f03ceb85ca812d9
alexchiriac <alexandru.chiriac@quant.network>
2019-10-07 16:37:45 +0100

    Update the transaction example with automatic sequence setting.

--- examples/a-to-b-transaction/a-to-b-transactions.js
+++ examples/a-to-b-transaction/a-to-b-transactions.js
@@ -9,9 +9,11 @@ const bpiKey = '<ENTER YOUR BPIKEY>';
 
 // Paste in your ethereum and ripple private keys.
 // For Ethereum you can generate an account using `OverledgerSDK.dlts.ethereum.createAccount` then fund the address at the Ropsten testnet faucet.
-const partyAEthereumPrivateKey = '39ed93f85c42b6e7655c2671b420065095f16f2210832bd61b9e27350049de52';
+const partyAEthereumPrivateKey = '0x5444c3e0f5e0ad11dfc5ff5252edad08940844eebaca5c85189103b5633e98cf';
+const partyAEthereumAddress = '0x88E4e574d4a45aBF8740F0048C491FA7B513207A'
 // For Ripple, you can go to the official ripple testnet faucet to get an account already funded.
-const partyARipplePrivateKey = 'snucuNobb6RNsgx85ujiZe2Npn5Su';
+const partyARipplePrivateKey = 'snGbMLSbreaeXDSRubW1hJ7x5jCYA';
+const partyARippleAddress = 'rGwnDLPBTTpFAYvd1LoKWyQzkUyKmf2LA9'
 
 
 const partyBEthereumAddress = '0x1a90dbb13861a29bFC2e464549D28bE44846Dbe4';
@@ -35,6 +37,13 @@ const partyBRippleAddress = 'rHVsZPVPjYJMR3Xa8YH7r7MapS7s5cyqgB';
     overledger.dlts.ethereum.setAccount(partyAEthereumPrivateKey);
     overledger.dlts.ripple.setAccount(partyARipplePrivateKey);
 
+    // Get the address sequences.
+    const ethereumSequenceRequest = await overledger.dlts.ethereum.getSequence(partyAEthereumAddress);
+    const rippleSequenceRequest = await overledger.dlts.ripple.getSequence(partyARippleAddress);
+    const ethereumAccountSequence = ethereumSequenceRequest.data.dltData[0].sequence;
+    const rippleAccountSequence = rippleSequenceRequest.data.dltData[0].sequence;
+
+
     // Sign the transactions.
     const signedTransactions = await overledger.sign([
     {
@@ -44,9 +53,9 @@ const partyBRippleAddress = 'rHVsZPVPjYJMR3Xa8YH7r7MapS7s5cyqgB';
       message: transactionMessage,
       options: {
         amount: '0', // On Ethereum you can send 0 amount transactions. But you still pay network fees
-        sequence: 1, // Sequence starts at 0 for newly created addresses
+        sequence: ethereumAccountSequence, // Sequence starts at 0 for newly created addresses
         feePrice: '1000', // Price for each individual gas unit this transaction will consume
-        feeLimit: '8000000', // The maximum fee that this transaction can use (can be set by the user)
+        feeLimit: '8000000', // The maximum fee that this transaction will use
       },
     },
     {
@@ -56,16 +65,14 @@ const partyBRippleAddress = 'rHVsZPVPjYJMR3Xa8YH7r7MapS7s5cyqgB';
       message: transactionMessage,
       options: {
         amount: '1', // Minimum allowed amount of drops is 1.
-        sequence: 1, // Sequence increases by 1 with each transaction and starts at 1 right after getting the address from the XRP testnet faucet.
+        sequence: rippleAccountSequence, // Sequence increases by 1 with each transaction and starts at 1 right after getting the address from the XRP testnet faucet.
         feePrice: '12', // Minimum feePrice on Ripple is 12 drops.
         maxLedgerVersion: '4294967295', // The maximum ledger version the transaction can be included in.
       },
     },]);
-    
-    console.log(signedTransactions[0].signedTransaction);
 
     // Send the transactions to Overledger.
-    //const result = (await overledger.send(signedTransactions)).data;
+    const result = (await overledger.send(signedTransactions)).data;
 
     // Log the result.
     console.log(JSON.stringify(result, null, 2));
