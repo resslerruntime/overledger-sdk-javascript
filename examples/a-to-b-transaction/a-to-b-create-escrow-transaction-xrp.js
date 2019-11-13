771046a02e0dafe60d4d558b4007776c03748642
Najla Chamseddine <najlachamseddine@Gilberts-MacBook.local>
2019-11-13 09:21:09 +0000

    integrate escrow transactions prototypes for ripple

---
+++ examples/a-to-b-transaction/a-to-b-create-escrow-transaction-xrp.js
@@ -0,0 +1,71 @@
+// Replace the dependency by @quantnetwork/overledger-bundle if you're in your own project
+const OverledgerSDK = require('../../packages/overledger-bundle/dist').default;
+
+//  ---------------------------------------------------------
+//  -------------- BEGIN VARIABLES TO UPDATE ----------------
+//  ---------------------------------------------------------
+const mappId = 'network.quant.software';
+const bpiKey = 'bpikeytest';
+
+// Paste in your ethereum and ripple private keys.
+// For Ethereum you can generate an account using `OverledgerSDK.dlts.ethereum.createAccount` then fund the address at the Ropsten Testnet Faucet.
+// For Ripple, you can go to the official Ripple Testnet Faucet to get an account already funded.
+const partyARipplePrivateKey = 'saw8418BRjNQkWiDak8szCySyDHvF';
+const partyARippleAddress = 'r38zfh4yzthZ51rGKzsGQgrUoQMiEzQx38'
+
+// Keep in mind that for Ripple, the minimum transfer amount is 20XRP (20,000,000 drops), if the address is not yet funded.
+const partyBRippleAddress = 'rP4MrmQcxnAAZusXGgnyZD4WR1Es5wjmLE';
+// const partyBRipplePrivateKey = 'sa3QfHx9dhMApSTJ5upjEC3ou3jhy';
+
+//  ---------------------------------------------------------
+//  -------------- END VARIABLES TO UPDATE ------------------
+//  ---------------------------------------------------------
+
+; (async () => {
+  try {
+    const overledger = new OverledgerSDK(mappId, bpiKey, {
+      dlts: [{ dlt: 'ripple' }],
+      provider: { network: 'testnet' },
+    });
+
+    const transactionMessage = 'Overledger JavaScript SDK Test';
+
+    // SET partyA accounts for signing;
+    overledger.dlts.ripple.setAccount(partyARipplePrivateKey);
+
+    // Get the address sequences.
+    const rippleSequenceRequest = await overledger.dlts.ripple.getSequence(partyARippleAddress);
+    const rippleAccountSequence = rippleSequenceRequest.data.dltData[0].sequence;
+
+
+    // Sign the transactions.
+    const signedTransactions = await overledger.sign([
+    {
+      // In order to prepare a ripple transaction offline, we have to specify a fee, sequence and maxLedgerVersion.
+      dlt: 'ripple',
+      toAddress: partyBRippleAddress,
+      message: transactionMessage,
+      options: {
+        amount: '100', // Minimum allowed amount of drops is 1.
+        sequence: rippleAccountSequence, // Sequence increases by 1 with each transaction and starts at 1 right after getting the address from the XRP testnet faucet.
+        feePrice: '12', // Minimum feePrice on Ripple is 12 drops.
+        maxLedgerVersion: '4294967295', // The maximum ledger version the transaction can be included in.
+        transactionType: 'ESCROW_CREATION',
+        atomicSwapParameters: {
+          allowCancelAfter: '1',
+          allowExecuteAfter: '0',
+          condition: 'Test'
+        }
+      },
+    },]);
+
+    // Send the transactions to Overledger.
+    const result = (await overledger.send(signedTransactions)).data;
+
+    // Log the result.
+    console.log(JSON.stringify(result, null, 2));
+
+  } catch (e) {
+    console.error('error:', e);
+  }
+})();
--- packages/overledger-dlt-ripple/src/Ripple.ts
+++ packages/overledger-dlt-ripple/src/Ripple.ts
@@ -5,6 +5,10 @@ import AbstractDLT from '@quantnetwork/overledger-dlt-abstract';
 import { Account, Options, TransactionOptions as BaseTransactionOptions } from '@quantnetwork/overledger-types';
 import { Payment } from 'ripple-lib/dist/npm/transaction/payment';
 import { Instructions } from 'ripple-lib/dist/npm/transaction/types';
+import { EscrowCreation } from 'ripple-lib/dist/npm/transaction/escrow-creation';
+import { EscrowExecution } from 'ripple-lib/dist/npm/transaction/escrow-execution';
+import { EscrowCancellation } from 'ripple-lib/dist/npm/transaction/escrow-cancellation';
+import * as cc from 'five-bells-condition';
 
 /**
  * @memberof module:overledger-dlt-ripple
@@ -79,6 +83,11 @@ class Ripple extends AbstractDLT {
    * @param {TransactionOptions} options
    */
   buildTransaction(toAddress: string, message: string, options: TransactionOptions): Transaction {
+    let payment;
+    let escrowCreation: EscrowCreation;
+    let escrowExecution: EscrowExecution;
+    let escrowCancellation: EscrowCancellation;
+    
     if (typeof options === 'undefined') {
       throw new Error('Transaction options must be defined.');
     }
@@ -103,33 +112,86 @@ class Ripple extends AbstractDLT {
     const feeInXRP = dropsToXrp(options.feePrice);
 
     const address = this.account.address;
-    const payment = {
-      source: {
-        address: this.account.address,
-        amount: {
-          value: amountInXRP,
-          currency: 'XRP',
+    if (options.transactionType === "PAYMENT") {
+      payment = {
+        source: {
+          address: this.account.address,
+          amount: {
+            value: amountInXRP,
+            currency: 'XRP',
+          },
         },
-      },
-      destination: {
-        address: toAddress,
-        minAmount: {
-          value: amountInXRP,
-          currency: 'XRP',
+        destination: {
+          address: toAddress,
+          minAmount: {
+            value: amountInXRP,
+            currency: 'XRP',
+          },
         },
-      },
-      memos: [{
-        data: message,
-      }],
-    };
+        memos: [{
+          data: message,
+        }],
+      };
+    }
     const instructions = {
       maxLedgerVersion,
       sequence: options.sequence,
       fee: feeInXRP,
     };
 
-    return { address, payment, instructions };
+    if (options.atomicSwapParameters) {
+      const params = options.atomicSwapParameters;
+      switch (options.transactionType) {
+        case "ESCROW_CREATION":
+          let paramsCreate = params as AtomicSwapCreateOptions;
+          escrowCreation = {
+            amount: options.amount,
+            destination: toAddress,
+            allowCancelAfter: this.computeCreateEscrowCancelAfter(paramsCreate.allowCancelAfter),
+            allowExecuteAfter: this.computeCreateEscrowCancelAfter(paramsCreate.allowExecuteAfter),
+            condition: this.computeEscrowConditionFulfillment(paramsCreate.condition).escrowCondition
+          }
+          console.log('escrowCreation', escrowCreation);
+          console.log('allowCancelAfter', escrowCreation.allowCancelAfter);
+          break;
+        case "ESCROW_EXECUTION":
+          let paramsExecute = params as AtomicSwapExecuteOptions;
+          escrowExecution = {
+            owner: address,
+            escrowSequence: parseInt(paramsExecute.escrowSequence),
+            condition: this.computeEscrowConditionFulfillment(paramsCreate.condition).escrowCondition,
+            fulfillment: this.computeEscrowConditionFulfillment(paramsCreate.condition).escrowFulfillment
+          }
+          console.log('escrowExecution', escrowExecution);
+          break;
+        case "ESCROW_CANCELLATION":
+          let paramsCancel = params as AtomicSwapCancelOptions;
+          escrowCancellation = {
+            owner: address,
+            escrowSequence: parseInt(paramsCancel.escrowSequence)
+          }
+          console.log('escrowCancellation', escrowCancellation);
+          break;
+      }
+    }
+
+    return { address, payment, instructions, escrowCreation, escrowExecution, escrowCancellation };
   }
+
+  computeEscrowConditionFulfillment(condition: string): { escrowCondition: string, escrowFulfillment: string } {
+    const preImageData = Buffer.from(condition, 'utf-8');
+    const myFulfillment = new cc.PreimageSha256();
+    myFulfillment.setPreimage(preImageData);
+    const escrowCondition = myFulfillment.getConditionBinary().toString('hex').toUpperCase();
+    const escrowFulfillment = myFulfillment.serializeBinary().toString('hex').toUpperCase();
+    return { escrowCondition, escrowFulfillment };
+  }
+
+  computeCreateEscrowCancelAfter(timeDelay: string): string {
+    let cancelAfter = new Date();
+    return new Date(cancelAfter.setHours(cancelAfter.getHours() + parseInt(timeDelay))).toISOString();
+  }
+
 
   /**
    * Sign the transaction
@@ -138,13 +200,32 @@ class Ripple extends AbstractDLT {
    * @param {string} message
    * @param {TransactionOptions} options
    */
-  _sign(toAddress: string, message: string, options: TransactionOptions): Promise<string> {
-    const built = this.buildTransaction(toAddress, message, options);
+  async _sign(toAddress: string, message: string, options: TransactionOptions): Promise<string> {
+    try {
+      const built = this.buildTransaction(toAddress, message, options);
+      let prepared;
+      console.log('transaction type', options.transactionType);
+      switch (options.transactionType) {
+        case "PAYMENT":
+          prepared = await this.rippleAPI.preparePayment(built.address, built.payment, built.instructions);
+          break;
+        case "ESCROW_CREATION":
+          prepared = await this.rippleAPI.prepareEscrowCreation(built.address, built.escrowCreation, built.instructions);
+          break;
+        case "ESCROW_EXECUTION":
+          prepared = await this.rippleAPI.prepareEscrowExecution(built.address, built.escrowExecution, built.instructions);
+          break;
+        case "ESCROW_CANCELLATION":
+          prepared = await this.rippleAPI.prepareEscrowCancellation(built.address, built.escrowCancellation, built.instructions)
+          break;
+        default:
+          prepared = await this.rippleAPI.preparePayment(built.address, built.payment, built.instructions);
+      }
+      return this.rippleAPI.sign(prepared.txJSON, this.account.privateKey).signedTransaction;
 
-    return this.rippleAPI.preparePayment(built.address, built.payment, built.instructions)
-      .then(
-        prepared => this.rippleAPI.sign(prepared.txJSON, this.account.privateKey).signedTransaction,
-      );
+    } catch (e) {
+      console.error(`Error while sending a ripple transaction`, e);
+    }
   }
 }
 
@@ -152,12 +233,44 @@ export type Transaction = {
   address: string,
   payment: Payment,
   instructions?: Instructions,
+  escrowCreation?: EscrowCreation,
+  escrowExecution?: EscrowExecution,
+  escrowCancellation?: EscrowCancellation
 };
 
 interface TransactionOptions extends BaseTransactionOptions {
   feePrice: string;
   maxLedgerVersion: string;
   amount: string;
+  transactionType: TransactionTypes;
+  atomicSwapParameters?: AtomicSwapCreateOptions | AtomicSwapExecuteOptions | AtomicSwapCancelOptions;
+}
+
+enum TransactionTypes {
+  payment = "PAYMENT",
+  escrowCreation = "ESCROW_CREATION",
+  escrowExecution = "ESCROW_EXECUTION",
+  escrowCancellation = "ESCROW_CANCELLATION"
+}
+
+interface AtomicSwapOptions { }
+
+interface AtomicSwapCreateOptions extends AtomicSwapOptions {
+  allowCancelAfter?: string; //in ISOString format
+  allowExecuteAfter?: string;
+  condition?: string; //this is the sha256 hashOutput
+}
+
+interface AtomicSwapExecuteOptions extends AtomicSwapOptions {
+  owner: string;
+  escrowSequence: string;
+  condition?: string;
+  fulfillment?: string;
+}
+
+interface AtomicSwapCancelOptions extends AtomicSwapOptions {
+  owner: string;
+  escrowSequence: string;
 }
 
 export default Ripple;
